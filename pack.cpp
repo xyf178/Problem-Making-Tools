283af40a73481bc252812aa90dfb1d6d1a9ae816
许越凡 <71132465+xyf178@users.noreply.github.com>
2022-01-01 15:00:06 +0800

    Add files via upload
    这是整理和包裹用的，在需要包裹的文件夹下运行 pack 即可

---
+++ pack.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#include<windows.h>
+using namespace std;
+#define int long long
+void fast(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+void file(string name){
+    freopen((name+".in").c_str(),"r",stdin);
+    freopen((name+".out").c_str(),"w",stdout);
+}
+vector<string>type={"pdf","tex","cpp","png","jpeg","jpg"};
+signed main(int argc,char**argv){
+    fast();
+//	file("");
+    system("rd pack");
+    system("md pack");
+    // system(((string)("cd ")+argv[2]).c_str());
+    for(auto x:type){
+        system(((string)("copy *.")+x+" pack\\*."+x).c_str());
+    }
+    return 0;
+}
