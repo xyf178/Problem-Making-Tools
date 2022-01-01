548f87a235a8f8469a65a6720174702f8bcc5b7c
许越凡 <71132465+xyf178@users.noreply.github.com>
2022-01-01 15:01:54 +0800

    Update pack.cpp

--- pack.cpp
+++ pack.cpp
@@ -11,7 +11,7 @@ void file(string name){
     freopen((name+".in").c_str(),"r",stdin);
     freopen((name+".out").c_str(),"w",stdout);
 }
-vector<string>type={"pdf","tex","cpp","png","jpeg","jpg"};
+vector<string>type={"pdf","tex","cpp","png","jpeg","jpg"};//这个是需要包裹的文件后缀，直接向后加即可
 signed main(int argc,char**argv){
     fast();
 //	file("");
